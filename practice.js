014898a4c297d91f14c94084032d6b0fa911fbb1
Sumit <thakursumitsinghrajputdelhi@gmail.com>
2025-02-02 10:49:28 +0530

    first

---
+++ practice.js
@@ -0,0 +1,5 @@
+let str= "Office politics is not good for the company the company management should take care of it";
+console.log(str.charAt(5));
+console.log(str.charCodeAt(5));
+console.log(str[5]);
+console.log(`length of string is ${str.length}`);
